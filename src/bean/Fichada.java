a23c428ca4dba7f58a5d9b60ecc49461a2e218a9
sofia <sofia@LAPTOP-FTT7462A>
2018-10-29 11:58:13 -0300

    atributos

--- src/bean/Fichada.java
+++ src/bean/Fichada.java
@@ -1,5 +1,24 @@
 package bean;
 
+import java.sql.Time;
+import java.util.*;
+
 public class Fichada {
 
+	Time horaEntrada;
+	Time horaSalida;
+	
+	Empleado empleado;
+	
+	
+	public void getHoraEntrada()
+	{
+		this.horaEntrada.getTime();
+	}
+	
+	public void getHoraSalida()
+	{
+		this.horaSalida.getTime();
+	}
+	
 }
