8014d094d93f03e7d4b7f4b4750ac1a0f4cad99a
KarT <KarT@DESKTOP-L5QE6FQ>
2018-10-29 12:13:16 -0300

    Commit Empleado - Carla

--- src/bean/Empleado.java
+++ src/bean/Empleado.java
@@ -1,5 +1,50 @@
 package bean;
 
-public class Empleado {
+import java.util.Date;
 
+public class Empleado {
+	private String nombre;
+	private String apellido;
+	private String mail;
+	private String dni;
+	private String telefono;
+	private Date fechaNac;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	
 }
