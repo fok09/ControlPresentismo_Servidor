d29ba2f43c74eaee515493a7747cff3f022f26b7
SMACTR <SMACTR@SMAP00000165.baires.techint.net>
2018-10-30 16:25:38 -0300

    Server version Final

--- src/server/Server.java
+++ src/server/Server.java
@@ -7,7 +7,6 @@ import remoteObjects.ControladorPresentismoRO;
 
 public class Server {
 	SistemaPresentismo objetoRemoto;
-//TDASistemaCine objetoRemoto;
 	
 	public static void main(String[] args)
 	{
@@ -20,20 +19,12 @@ public class Server {
 	
     public void iniciar() {
     	try {
-<<<<<<< HEAD
-//    		LocateRegistry.createRegistry(1099);	
-
-			//e.printStackTrace();
-		}finally {
-			
-=======
     		LocateRegistry.createRegistry(1099);
     		SistemaPresentismo controladorPresentismo = new ControladorPresentismoRO();
     		Naming.rebind ("//localhost/ControladorPresentismo", controladorPresentismo);
             System.out.println("Fijado en //localhost/ControladorPresentismo");
 		} catch (Exception e) {
 			e.printStackTrace();
->>>>>>> refs/remotes/origin/master
 		}
-
-    }}
+    }
+}
